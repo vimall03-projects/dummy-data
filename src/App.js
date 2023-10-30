9aec37b2efafb963dbfdfb1d3362233a1bc14e33
Vimal M <109758195+Vimall03@users.noreply.github.com>
2023-10-30 23:23:01 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch(`https://dummyjson.com/users`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // Add this line for debugging
+        setData(data.users);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Dummy data</h1>
+      <table className="data-table">
+        <tbody>
+          <tr>
+            <th>SNo</th>
+            <th>Profile Pic</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>E-mail</th>
+            <th>Username</th>
+            <th>Domain</th>
+            <th>IP</th>
+            <th>University</th>
+          </tr>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>
+                <img src={item.image} alt={item.firstName} />
+              </td>
+              <td>{item.firstName}</td>
+              <td>{item.lastName}</td>
+              <td>{item.gender}</td>
+              <td>{item.email}</td>
+              <td>{item.username}</td>
+              <td>{item.domain}</td>
+              <td>{item.ip}</td>
+              <td>{item.university}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
